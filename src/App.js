0f81da664e2f083b53c607ed766756b77e4730f8
robinwolter-HB <robin.wolter@icloud.com>
2026-04-13 18:35:00 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from 'react-router-dom'
 import { AuthProvider, useAuth } from './lib/AuthContext'
 import './index.css'
