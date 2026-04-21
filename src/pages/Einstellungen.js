d412887bf87e2c9f51f1bf798c8460e51558bffc
robinwolter-HB <robin.wolter@icloud.com>
2026-04-21 21:43:19 +0200

    Update Einstellungen.js

--- src/pages/Einstellungen.js
+++ src/pages/Einstellungen.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
 import { useAuth } from '../lib/AuthContext'
 
